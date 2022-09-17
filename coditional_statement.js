103cf2078d0f32e2b25058129119b0a77c96c1e1
Ankushkushwaha0009 <93570548+Ankushkushwaha0009@users.noreply.github.com>
2022-09-17 23:50:59 +0530

    Create coditional_statement.js

---
+++ coditional_statement.js
@@ -0,0 +1,89 @@
+// num = parseInt(prompt("Enter the Number"))
+// if(num%2 == 0){
+//     console.log("no is even ")
+// }
+// else{
+//     console.log("no is odd")
+// }
+
+
+// let a = 10
+// let b = 21
+// let c = 4
+
+// if(a > b && a>c){
+//     console.log("a is greater");
+// }
+// else if(b > a &&  b > c){
+//     console.log("b is greater")
+// }
+// else{
+//     console.log("c is greater ")
+// }
+// console.log(max(a,b))
+
+// let n = parseint(prompt("Enter the Number :"))
+
+// n = 5
+// k = 10
+// for(let i = 1 ; i <= 10 ; i++){
+//      console.log(`${n} * ${i} = ${n*i}  \t   ${n} * ${k} = ${n*k} `)
+//      k--;
+//   }
+
+// for(let i = 1 ; i <= 100 ; i++){
+//       if(i%3 == 0 && i%5 == 0 ){
+//         console.log("FIZZ BUZZ")
+//       }
+//       else if(i%3 == 0){
+//         console.log("FIZZ");
+//       }
+//       else if(i%5 == 0){
+//         console.log("BUZZ");
+//       }
+//       else{
+//         console.log(i)
+//       }
+// }
+
+
+//genrate a number betwenn any interaval 
+
+
+// let x2  = Math.floor(Math.random() * (101-1)) + 1;
+// // console.log("random number is   : " , x2)
+// let us = false;
+// l = prompt("Enter the  difficult level (hard -> 3) (easy -> 1) (medium -> 2) :")
+// let n  =  10
+// if(l == 1)  n = 4 ;
+// else if(l == 2)  n = 6;
+// else
+//   n = 8 ;
+
+// for(let i = 1 ; i <= n ; i++){
+//     let x3  = prompt(" Enter the Number :" ); 
+//     if(x3 > x2)console.log(" Please enter the smaller number :");
+//     else if(x2 > x3) console.log("please enter the graeter number ");
+//     else if(x2 == x3){
+//       us  = true
+//       alert("you won !")
+//       break;
+//     }
+
+// }
+
+// if(!us)
+//   alert("you loss the game ")
+
+// n = 5 ;
+// f = 1 ;
+// for(let i = 1 ; i<=n ; i++ ){
+//      f = f*i ; 
+// }
+// console.log("factorial of number is :", f)
+
+// for(let i = 1 ; i <= 20 ; i++){
+//      if(i == 13)  continue;
+//      else
+//          console.log("Floor No  is :" , i)
+// }
